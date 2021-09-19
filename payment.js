0afeca4d23cfd4ecb02c6ddf5721123c3cc0ed63
Akash-Agrawa-l <68943987+Akash-Agrawa-l@users.noreply.github.com>
2021-09-19 19:52:48 +0530

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,21 @@
+function payment() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var amount = document.getElementById('amount').value;
+    // var mode = document.getElementById('mode').value;
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: `softtechindustries2@gmail.com`,
+        Password: `softtech.io`,
+        To: `${email}`,
+        From: "softtechindustries2@gmail.com",
+        Subject: `Donation Successful!`,
+        Body: `Hi ${name},<br><br>
+        Thank you very much for donating an amount of <b>Rs. ${amount}</b> to SoftTech Industries. We truly and sincerely appreciate your support of our mission.<br><br>
+        Thanking you. <br><br>
+        Yours faithfully,<br>
+        Akash Agrawal<br>
+        Founder - SoftTech Industries`,
+    }).then((message) => alert("Payment successful"));
+}
