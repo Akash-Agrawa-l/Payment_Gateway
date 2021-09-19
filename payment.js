1d87b1f2fb6f8280390d6f4cae83bf00f6fe87e2
Akash-Agrawa-l <68943987+Akash-Agrawa-l@users.noreply.github.com>
2021-09-19 19:57:29 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -7,7 +7,7 @@ function payment() {
     Email.send({
         Host: "smtp.gmail.com",
         Username: `softtechindustries2@gmail.com`,
-        Password: `softtech.io`,
+        Password: `SOFTTECHIND`,
         To: `${email}`,
         From: "softtechindustries2@gmail.com",
         Subject: `Donation Successful!`,
